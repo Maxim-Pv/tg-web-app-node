b6c9b43eee7826cd9e282630c7783b0cd60f3d0d
Maksim Pavlov <maxim.paw2010@yandex.ru>
2024-05-12 13:02:53 +0300

    init

---
+++ index.js
@@ -0,0 +1,83 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
+
+const token = '7184151585:AAE0RMyGxPOH_vG3Lc79Db0kzqleS6WC6CY'
+const webAppUrl = 'https://cozy-sopapillas-4040fb.netlify.app';
+
+const bot = new TelegramBot(token, {polling: true});
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (text === '/start') {
+    await bot.sendMessage(chatId, 'Ниже появится кнопка, Fill in the form', {
+        reply_markup: {
+            keyboard: [
+                [{text: 'Fill in the form', web_app: {url: webAppUrl + '/form'}}]
+            ]
+        }
+    });
+
+    await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+        reply_markup: {
+            inline_keyboard: [
+                [{text: 'Сreate an order', web_app: {url: webAppUrl}}]
+            ]
+        }
+    });
+
+  }
+
+  if (msg?.web_app_data?.data) {
+    try {
+        const data = JSON.parse(msg?.web_app_data?.data)
+        console.log(data)
+        await bot.sendMessage(chatId, 'Thanks for the feedback!')
+        await bot.sendMessage(chatId, 'Your county: ' + data?.country);
+        await bot.sendMessage(chatId, 'Your street: ' + data?.street);
+
+        setTimeout(async () => {
+            await bot.sendMessage(chatId, 'You will get all the information in this chat');
+        }, 3000)
+
+    } catch (e) {
+        console.log(e);
+    }
+  }
+});
+
+app.post('/web-data', async (req, res) => {
+    const {queryId, products, totalPrice} = req.body;
+    try {    
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Thank you for your order',
+            input_message_content: {
+                message_text: `Your order: ${products} Total: ${totalPrice}`
+            }
+        });
+        return res.status(200).json({message: 'Success'});
+
+    } catch (e) {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Error',
+            input_message_content: {
+                message_text: `Error: ${e.message}`
+            }
+        });
+        return res.status(500).json({error: e.message});
+    }
+})
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
